99fe9bb101b4d9e217bf577455fcc6498291abb1
YeoSeungA <silo1116@naver.com>
2025-01-25 15:24:01 +0900

    api-docmentation 브라우저 실현

--- src/test/java/com/springboot/homework/MemberControllerDocumentationTest.java
+++ src/test/java/com/springboot/homework/MemberControllerDocumentationTest.java
@@ -30,8 +30,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
@@ -91,6 +90,7 @@ public class MemberControllerDocumentationTest {
 //                        ),
                         responseFields(
                                 List.of(
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("데이터"),
                                         fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("멤버 ID"),
                                         fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
                                         fieldWithPath("data.name").type(JsonFieldType.STRING).description("이름"),
@@ -105,19 +105,67 @@ public class MemberControllerDocumentationTest {
     @Test
     public void getMembersTest() throws Exception {
 //        given
-        long memberId = 1L;
-        Page<Member> pageMembers = new PageImpl<>(List.of(new Member()));
+        Page<Member> pageMembers = new PageImpl<>(List.of(new Member(),new Member(),new Member()));
         List<MemberDto.Response> responses = List.of(
-                new MemberDto.Response(memberId, "bana@rabbit.com", "바니","010-1234-5678", Member.MemberStatus.MEMBER_ACTIVE, new Stamp()),
-                new MemberDto.Response(memberId, "bana@preso.com", "바나","010-1234-5670", Member.MemberStatus.MEMBER_ACTIVE, new Stamp()),
-                new MemberDto.Response(memberId, "bani@rabbit.com", "바나나","010-1234-5679", Member.MemberStatus.MEMBER_ACTIVE, new Stamp())
+                new MemberDto.Response(1L, "bana@rabbit.com", "바니","010-1234-5678", Member.MemberStatus.MEMBER_ACTIVE, new Stamp()),
+                new MemberDto.Response(2L, "bana@preso.com", "바나","010-1234-5670", Member.MemberStatus.MEMBER_ACTIVE, new Stamp()),
+                new MemberDto.Response(3L, "bani@rabbit.com", "바나나","010-1234-5679", Member.MemberStatus.MEMBER_ACTIVE, new Stamp())
         );
 
         given(memberService.findMembers(Mockito.anyInt(),Mockito.anyInt())).willReturn(pageMembers);
         given(mapper.membersToMemberResponses(Mockito.anyList())).willReturn(responses);
 //        when
-
+        ResultActions actions = mockMvc.perform(
+                get("/v11/members")
+                        .param("page", "1")
+                        .param("size","3")
+                        .accept(MediaType.APPLICATION_JSON)
+        );
 //        then
+        actions.andExpect(status().isOk())
+//                .andExpect(jsonPath("$.data[0].name").value(responses.get(0).getName()))
+                .andExpect(jsonPath("$.data.length()").value(responses.size()))
+                .andDo(document(
+                        "get-Members",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        requestParameters(
+                                parameterWithName("page").description("조회할 페이지 번호"),
+                                parameterWithName("size").description("페이지 당 데이터 수")
+                        ),
+                        responseFields(
+                                List.of(
+//                                        fieldWithPath("data[0].memberId").type(JsonFieldType.NUMBER).description("멤버 Id"),
+//                                        fieldWithPath("data[0].email").type(JsonFieldType.STRING).description("이메일"),
+//                                        fieldWithPath("data[0].name").type(JsonFieldType.STRING).description("이름"),
+//                                        fieldWithPath("data[0].phone").type(JsonFieldType.STRING).description("핸드폰 번호"),
+//                                        fieldWithPath("data[0].memberStatus").type(JsonFieldType.STRING).description("회원 상태"),
+//                                        fieldWithPath("data[0].stamp").type(JsonFieldType.NUMBER).description("스탬프 갯수"),
+//                                        fieldWithPath("data[1].memberId").type(JsonFieldType.NUMBER).description("멤버 Id"),
+//                                        fieldWithPath("data[1].email").type(JsonFieldType.STRING).description("이메일"),
+//                                        fieldWithPath("data[1].name").type(JsonFieldType.STRING).description("이름"),
+//                                        fieldWithPath("data[1].phone").type(JsonFieldType.STRING).description("핸드폰 번호"),
+//                                        fieldWithPath("data[1].memberStatus").type(JsonFieldType.STRING).description("회원 상태"),
+//                                        fieldWithPath("data[1].stamp").type(JsonFieldType.NUMBER).description("스탬프 갯수"),
+//                                        fieldWithPath("pageInfo.page").type(JsonFieldType.NUMBER).description("page"),
+//                                        fieldWithPath("pageInfo.size").type(JsonFieldType.NUMBER).description("size"),
+//                                        fieldWithPath("pageInfo.totalElements").type(JsonFieldType.NUMBER).description("totalElements"),
+//                                        fieldWithPath("pageInfo.totalPages").type(JsonFieldType.NUMBER).description("totalPages")
+                                        fieldWithPath("data").type(JsonFieldType.ARRAY).description("데이터"),
+                                        fieldWithPath("data[].memberId").type(JsonFieldType.NUMBER).description("멤버 Id"),
+                                        fieldWithPath("data[].email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("data[].name").type(JsonFieldType.STRING).description("이름"),
+                                        fieldWithPath("data[].phone").type(JsonFieldType.STRING).description("핸드폰 번호"),
+                                        fieldWithPath("data[].memberStatus").type(JsonFieldType.STRING).description("회원 상태"),
+                                        fieldWithPath("data[].stamp").type(JsonFieldType.NUMBER).description("스탬프 갯수"),
+                                        fieldWithPath("pageInfo").type(JsonFieldType.OBJECT).description("페이지 Info"),
+                                        fieldWithPath("pageInfo.page").type(JsonFieldType.NUMBER).description("page"),
+                                        fieldWithPath("pageInfo.size").type(JsonFieldType.NUMBER).description("size"),
+                                        fieldWithPath("pageInfo.totalElements").type(JsonFieldType.NUMBER).description("totalElements"),
+                                        fieldWithPath("pageInfo.totalPages").type(JsonFieldType.NUMBER).description("totalPages")
+                                )
+                        )
+                ));
     }
 
     @Test
